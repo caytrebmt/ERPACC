33299856479fa935fc052af02967c77ba5774890
caytrebmt <56768061+caytrebmt@users.noreply.github.com>
2026-08-17 14:47:43 +0700

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -93,7 +93,7 @@ class Config:
 
     _raw_cors_origins = os.getenv('SHOP_CORS_ORIGINS', '')
     SHOP_CORS_ORIGINS = [o.strip() for o in _raw_cors_origins.split(',') if o.strip()]
-
+    SHOP_BACKEND_URL = os.getenv('SHOP_BACKEND_URL', '').rstrip('/')
     GOOGLE_OAUTH_CLIENT_ID = os.getenv('GOOGLE_OAUTH_CLIENT_ID', '')
     GOOGLE_OAUTH_CLIENT_SECRET = os.getenv('GOOGLE_OAUTH_CLIENT_SECRET', '')
     GOOGLE_OAUTH_REDIRECT_URI = os.getenv('GOOGLE_OAUTH_REDIRECT_URI', 'http://localhost:5000/auth/google/callback')
